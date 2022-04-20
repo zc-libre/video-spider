e5605d8fb9275cff702b7b1ab5bb8ce4d8ecf41c
libre <zc150622@gmail.com>
2022-04-20 23:00:46 +0800

    :sparkles: 增加日志打印, 错误请求增加创建时间

--- src/main/java/com/libre/video/pojo/ErrorVideo.java
+++ src/main/java/com/libre/video/pojo/ErrorVideo.java
@@ -3,13 +3,17 @@ package com.libre.video.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.libre.core.time.DatePattern;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @TableName("error_video")
 public class ErrorVideo {
 
-    @TableId(type = IdType.ASSIGN_ID)
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     private String url;
@@ -19,4 +23,7 @@ public class ErrorVideo {
     private String html;
 
 	private Integer requestType;
+
+	@JsonFormat(pattern = DatePattern.NORM_DATETIME_PATTERN)
+	private LocalDateTime createTime;
 }
