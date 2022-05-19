a392c357fd462d448f0753e1345dd3e1398c58d2
libre <zc150622@gmail.com>
2022-05-20 02:11:54 +0800

    :rocket: 视频下载观看优化

--- src/main/java/com/libre/video/core/download/M3u8Download.java
+++ src/main/java/com/libre/video/core/download/M3u8Download.java
@@ -1,41 +1,38 @@
 package com.libre.video.core.download;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Streams;
 import com.google.common.io.Closeables;
 import com.libre.core.exception.LibreException;
-import com.libre.core.toolkit.CollectionUtil;
 import com.libre.core.toolkit.StringPool;
-import com.libre.core.toolkit.StringUtil;
 import com.libre.video.config.VideoProperties;
 import com.libre.video.core.enums.RequestTypeEnum;
-import com.libre.video.core.event.VideoUploadEvent;
 import com.libre.video.core.event.VideoEventPublisher;
+import com.libre.video.core.event.VideoUploadEvent;
 import com.libre.video.pojo.Video;
 import com.libre.video.toolkit.ThreadPoolUtil;
 import com.libre.video.toolkit.VideoFileUtils;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
-import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
-import java.io.*;
-import java.nio.file.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -71,27 +68,27 @@ public class M3u8Download {
 		VideoEventPublisher.publishVideoUploadEvent(new VideoUploadEvent(true, video, resource));
 	}
 
-	@Async("downloadExecutor")
 	public void downloadM3u8FileToLocal(InputStream inputStream, Video video) {
 		String tempDir = createDirectory(video.getId());
-		String m3u8FileName = tempDir + File.separator +  "index.m3u8";
+		String m3u8FileName = tempDir + File.separator + "index.m3u8";
 		Path m3u8FilePath = Paths.get(m3u8FileName);
 		if (Files.exists(m3u8FilePath)) {
 			return;
 		}
 		List<String> lines = copyM3u8File(inputStream, m3u8FilePath);
-		lines = lines.stream().filter(line -> line.endsWith(TS_SUFFIX)).collect(Collectors.toList());
-		String realUrl = video.getRealUrl();
-		String baseUrl = realUrl.substring(0, realUrl.lastIndexOf(StringPool.SLASH));
-
-		if (RequestTypeEnum.REQUEST_BA_AV.getType() == video.getVideoWebsite()) {
-			downloadTsFilesAsync(baseUrl, tempDir, lines);
-		}
-		else if (RequestTypeEnum.REQUEST_9S.getType() == video.getVideoWebsite()) {
-			downloadTsFiles(baseUrl, tempDir, lines);
-		}
+		ThreadPoolTaskExecutor executor = ThreadPoolUtil.downloadExecutor();
+	     executor.execute(() -> {
+			 List<String> tsLines = lines.stream().filter(line -> line.endsWith(TS_SUFFIX)).collect(Collectors.toList());
+			 String realUrl = video.getRealUrl();
+			 String baseUrl = realUrl.substring(0, realUrl.lastIndexOf(StringPool.SLASH));
+			 if (RequestTypeEnum.REQUEST_BA_AV.getType() == video.getVideoWebsite()) {
+				 downloadTsFilesAsync(baseUrl, tempDir, tsLines);
+			 }
+			 else if (RequestTypeEnum.REQUEST_9S.getType() == video.getVideoWebsite()) {
+				 downloadTsFiles(baseUrl, tempDir, tsLines);
+			 }
+		 });
 	}
-
 
 	public void downloadTsFiles(String baseUrl, String tempDir, List<String> lines) {
 		for (String ts : lines) {
@@ -109,17 +106,17 @@ public class M3u8Download {
 	}
 
 	public void downloadTsFilesAsync(String baseUrl, String tempDir, List<String> lines) {
-		String[] urls = lines.toArray(new String[0]);
-		Flux.just(urls)
-				.flatMap(ts -> webClient.get().uri(baseUrl + StringPool.SLASH + ts)
-						.accept(MediaType.APPLICATION_OCTET_STREAM).retrieve().bodyToMono(Resource.class).retry(5)
-						.doOnError(e -> log.error("请求异常")).publishOn(Schedulers.boundedElastic()).map(resource -> {
-							String tsPath = tempDir + File.separator + ts;
-							log.info("正在下载： {}", tsPath);
-							copyTsFile(tsPath, resource);
-							return resource;
-						}))
-				.collectList().doOnError(e -> log.error("请求异常")).doOnNext(list -> log.info("所有请求完成")).block();
+		Flux.fromIterable(lines).flatMap(ts -> {
+			Mono<Resource> mono = webClient.get().uri(baseUrl + StringPool.SLASH + ts).accept(MediaType.APPLICATION_OCTET_STREAM)
+				.retrieve().bodyToMono(Resource.class).retry(5).doOnError(e -> log.error("请求异常"))
+				.publishOn(Schedulers.boundedElastic()).map(resource -> {
+					String tsPath = tempDir + File.separator + ts;
+					log.info("正在下载： {}", tsPath);
+					copyTsFile(tsPath, resource);
+					return resource;
+				});
+			return mono;
+		}).collectList().doOnError(e -> log.error("请求异常")).doOnNext(list -> log.info("所有请求完成")).block();
 
 	}
 
