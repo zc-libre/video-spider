2022b623c3af333b34e114931bca32bea8010b2a
libre <zc150622@gmail.com>
2022-05-20 02:12:36 +0800

    :wrench: 部署脚本修改

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 tar -zxvf package.tgz
-tar -zxvf app-app.tar.gz
+tar -zxvf video-spider-1.0.0-app.tar.gz
 docker stop app
 docker rm app
-cd /root/app
-rm -rf maven* classes
+cd /root/app/
+rm -rf maven* classes arc* generated-sources lib
+cd video-spider-1.0.0/bin
 sudo docker rmi app:1.0
 docker build -t app:1.0 .
 docker run -it --name app \
