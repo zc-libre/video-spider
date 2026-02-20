41c4232375c85cbd5a7da8371b135da8e1fb72e4
Libre <zc150622@gmail.com>
2026-02-20 19:10:54 +0800

    🐛 修复爬虫接口同步阻塞导致前端超时，启用 @EnableAsync 激活异步执行

--- src/main/java/com/libre/video/VideoSpiderApplication.java
+++ src/main/java/com/libre/video/VideoSpiderApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
 @EnableBatchProcessing
 @EnableEncryptableProperties
 @SpringBootApplication
